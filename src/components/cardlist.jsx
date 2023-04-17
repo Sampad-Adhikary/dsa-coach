321cb5752b7fc939b199c8af957996528e217bfa
Sampad Adhikary <sampadadhikary031@gmail.com>
2023-04-18 01:49:58 +0530

    bug fix

--- src/components/cardlist.jsx
+++ src/components/cardlist.jsx
@@ -6,22 +6,22 @@ export default function CardGroup(){
         <div className="cardList">
             <h4 className="hero4">Topics</h4>
             <div className="row row-cols-1 row-cols-md-3  lg-4 card-row ">
-            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-12 cardItem"><Card/></div>
-            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-12 cardItem"><Card/></div>
-            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-12 cardItem"><Card/></div>
-            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-12 cardItem"><Card/></div>
-            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-12 cardItem"><Card/></div>
-            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-12 cardItem"><Card/></div>
-            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-12 cardItem"><Card/></div>
-            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-12 cardItem"><Card/></div>
-            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-12 cardItem"><Card/></div>
-            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-12 cardItem"><Card/></div>
-            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-12 cardItem"><Card/></div>
-            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-12 cardItem"><Card/></div>
-            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-12 cardItem"><Card/></div>
-            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-12 cardItem"><Card/></div>
-            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-12 cardItem"><Card/></div>
-            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-12 cardItem"><Card/></div>
+            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-6 cardItem"><Card/></div>
+            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-6 cardItem"><Card/></div>
+            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-6 cardItem"><Card/></div>
+            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-6 cardItem"><Card/></div>
+            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-6 cardItem"><Card/></div>
+            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-6 cardItem"><Card/></div>
+            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-6 cardItem"><Card/></div>
+            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-6 cardItem"><Card/></div>
+            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-6 cardItem"><Card/></div>
+            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-6 cardItem"><Card/></div>
+            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-6 cardItem"><Card/></div>
+            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-6 cardItem"><Card/></div>
+            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-6 cardItem"><Card/></div>
+            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-6 cardItem"><Card/></div>
+            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-6 cardItem"><Card/></div>
+            <div class="col-xl-3 col-md-4 col-lg-4 col-sm-6 cardItem"><Card/></div>
             </div>
         </div>
     );
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
     <div className="hero">
         <div className="row heroRow">
       <div className="col-lg-6 col-md-12 col-sm-12 heroImg">
-        <img src="resources/hero.png" width="600px" alt="DSA-Coach" />
+        <img className="heroImg" src="resources/hero.png" width="600px" alt="DSA-Coach" />
       </div>
       <div className="col-lg-6 col-md-12 col-sm-12 heroText">
         <h4 className="hero4">Want to Learn DSA ?</h4>
