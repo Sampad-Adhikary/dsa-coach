758102b0f2a9c4ef3a20141f990eed70bb768048
Sampad Adhikary <sampadadhikary031@gmail.com>
2023-04-19 02:58:21 +0530

    ui fix

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg myNav">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#hero"><img src="/resources/home.png" width="50px" alt="home"/></a>
+    <a className="navbar-brand" href="https://sampadadhikary.cyclic.app/" target="blank"><img src="/resources/home.png" width="50px" alt="home"/></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
